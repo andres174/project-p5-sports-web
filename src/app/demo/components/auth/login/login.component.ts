c44db40efa2bf4e89f1085f5fc7030ba26c69bd1
andres174 <zambrasda17@gmail.com>
2023-04-01 23:29:15 -0500

    avance login 1

--- src/app/demo/components/auth/login/login.component.ts
+++ src/app/demo/components/auth/login/login.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { error } from 'console';
+import { LoginService } from 'src/app/demo/service/login.service';
 import { LayoutService } from 'src/app/layout/service/app.layout.service';
 
 @Component({
@@ -13,11 +15,41 @@ import { LayoutService } from 'src/app/layout/service/app.layout.service';
         }
     `]
 })
-export class LoginComponent {
+export class LoginComponent implements  OnInit {
 
     valCheck: string[] = ['remember'];
 
     password!: string;
+    email!: string;
 
-    constructor(public layoutService: LayoutService) { }
+    constructor(public layoutService: LayoutService,
+        private login_service: LoginService) { }
+
+    ngOnInit(): void {
+        
+    }
+
+    login(){
+        console.log(this.password, this.email);
+
+        let data = {
+            email: this.email,
+            password: this.password
+        }
+
+        this.login_service.login(data).subscribe({
+            next: (res) => {
+                console.log(res);
+                localStorage.setItem('id', res.id.toString());
+                localStorage.setItem('typeUserId', res.typeUserId.toString());
+                localStorage.setItem('user', res.userName);
+                localStorage.setItem('email', res.email);
+                localStorage.setItem('token', res.accesToken);
+                localStorage.setItem('rol', res.rol);
+            },
+            error: (err) => {
+                console.log(err);
+            }
+        });
+    }
 }
