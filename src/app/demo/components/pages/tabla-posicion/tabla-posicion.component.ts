86d3672e3b6eead8a4e5e1bfd65b47e4af87a4f1
Luis Macias <luismaciasantonio2000@gmail.com>
2023-04-14 13:34:28 -0500

    solo falta el html en tabla de posiciones

--- src/app/demo/components/pages/tabla-posicion/tabla-posicion.component.ts
+++ src/app/demo/components/pages/tabla-posicion/tabla-posicion.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { ResultadoService } from 'src/app/demo/service/resultado.service';
+import { TablaPosicion } from './tabla-posicion';
+import { MessageService } from 'primeng/api';
 @Component({
   selector: 'app-tabla-posicion',
   templateUrl: './tabla-posicion.component.html',
-  styleUrls: ['./tabla-posicion.component.scss']
+  styleUrls: ['./tabla-posicion.component.scss'],
+  providers: [MessageService],
 })
-export class TablaPosicionComponent {
+export class TablaPosicionComponent implements OnInit {
+  Tablaposicion: TablaPosicion[] = [];
+ 
+  constructor(
+    private ResultadoService: ResultadoService,
+    private messageService: MessageService,
+  ) {
+   /*  this.url = environment.url; */
+  }
+  ngOnInit(){
+  }
+
+  getTablePosition(event:any){
+    this.ResultadoService.MostrarTablePosition(event.detail.value).subscribe({
+      next:(res)=> this.Tablaposicion = res,
+      error:(err)=>console.log(err)
+    });
+  }
+
+  successMessage(msg: string) {
+    this.messageService.add({
+      severity: "success",
+      summary: "Acción exitosa",
+      detail: msg,
+      life: 3000,
+    });
+  }
+
+  errorMessage(msg: string) {
+    this.messageService.add({
+      severity: "error",
+      summary: "Ocurrio un Error",
+      detail: msg,
+      life: 3000,
+    });
+  }
 
 }
