6164774be42feea7662d64dbc8cdf10e2251d9fb
StevanVS <stevanvs@outlook.com>
2023-04-17 08:35:54 -0500

    Eliminar modulos innecesarios

--- src/app/demo/components/pages/equipos/equipos.module.ts
+++ src/app/demo/components/pages/equipos/equipos.module.ts
@@ -7,41 +7,29 @@ import { EquiposComponent } from "./equipos.component";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { ButtonModule } from "primeng/button";
 import { DialogModule } from "primeng/dialog";
-import { DropdownModule } from "primeng/dropdown";
-import { FileUploadModule } from "primeng/fileupload";
-import { InputNumberModule } from "primeng/inputnumber";
 import { InputTextModule } from "primeng/inputtext";
-import { InputTextareaModule } from "primeng/inputtextarea";
-import { PasswordModule } from "primeng/password";
-import { RadioButtonModule } from "primeng/radiobutton";
-import { RatingModule } from "primeng/rating";
 import { RippleModule } from "primeng/ripple";
 import { TableModule } from "primeng/table";
 import { ToastModule } from "primeng/toast";
 import { ToolbarModule } from "primeng/toolbar";
 import { ImageModule } from "primeng/image";
+import { FileUploadModule } from "primeng/fileupload";
 
 @NgModule({
   declarations: [EquiposComponent],
   imports: [
     CommonModule,
     EquiposRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
     TableModule,
     FileUploadModule,
-    FormsModule,
     ButtonModule,
     RippleModule,
     ToastModule,
     ToolbarModule,
-    RatingModule,
     InputTextModule,
-    InputTextareaModule,
-    DropdownModule,
-    RadioButtonModule,
-    InputNumberModule,
     DialogModule,
-    ReactiveFormsModule,
-    PasswordModule,
     ImageModule,
   ],
 })
--- src/app/demo/components/pages/usuarios/usuarios.module.ts
+++ src/app/demo/components/pages/usuarios/usuarios.module.ts
@@ -6,13 +6,8 @@ import { UsuariosComponent } from "./usuarios.component";
 import { FormsModule } from "@angular/forms";
 import { ButtonModule } from "primeng/button";
 import { DialogModule } from "primeng/dialog";
-import { DropdownModule } from "primeng/dropdown";
 import { FileUploadModule } from "primeng/fileupload";
-import { InputNumberModule } from "primeng/inputnumber";
 import { InputTextModule } from "primeng/inputtext";
-import { InputTextareaModule } from "primeng/inputtextarea";
-import { RadioButtonModule } from "primeng/radiobutton";
-import { RatingModule } from "primeng/rating";
 import { RippleModule } from "primeng/ripple";
 import { TableModule } from "primeng/table";
 import { ToastModule } from "primeng/toast";
@@ -20,28 +15,22 @@ import { ToolbarModule } from "primeng/toolbar";
 import { ReactiveFormsModule } from "@angular/forms";
 import { PasswordModule } from "primeng/password";
 import { ImageModule } from "primeng/image";
-import { BlockUIModule } from "primeng/blockui";
 
 @NgModule({
   declarations: [UsuariosComponent],
   imports: [
     CommonModule,
     UsuariosRoutingModule,
+    ReactiveFormsModule,
+    FormsModule,
     TableModule,
     FileUploadModule,
-    FormsModule,
     ButtonModule,
     RippleModule,
     ToastModule,
     ToolbarModule,
-    RatingModule,
     InputTextModule,
-    InputTextareaModule,
-    DropdownModule,
-    RadioButtonModule,
-    InputNumberModule,
     DialogModule,
-    ReactiveFormsModule,
     PasswordModule,
     ImageModule,
   ],
